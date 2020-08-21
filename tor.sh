8b9cc7ab57efe5b0d6410091a30191220696a5ff
arifstyawira <arifstyawira@gmail.com>
2020-08-21 09:50:49 +0700

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -87,6 +87,18 @@ sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sudo iptables-save | sudo tee /etc/iptables.conf
 mv /etc/rc.local  /etc/rc.local.bak
 echo "
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
 iptables-restore < /etc/iptables.conf
 exit 0" >> /etc/rc.local
 
